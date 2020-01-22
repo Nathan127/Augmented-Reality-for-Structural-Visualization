6c31c82a948d4cde11cf072cefdfc7e81f1f73b2
Nathan DeStafeno <32209187+Nathan127@users.noreply.github.com>
2020-01-21 16:32:06 -0800

    Pushing Unity template making project

---
+++ HoloLens Maker/Assets/Scripts/Indicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour
+{
+    public string sensorName;
+    public float value;
+    public Vector3 position;
+
+    public float valueMax;
+    public float valueMin;
+
+    [Range(1.0f, 10.0f)]
+    public float size = 1;
+    //ToDo, fade between two colors
+    public float color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = position;
+        transform.localScale += new Vector3(1, 1, 1) * size;
+    }
+}

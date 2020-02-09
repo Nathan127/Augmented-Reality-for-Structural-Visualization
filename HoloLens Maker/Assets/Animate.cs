ae0320927049acb898f89582cc5db9f5390bab87
Justin Eckert <eckertl@oregonstate.edu>
2020-02-08 16:17:36 -0800

    Pull in MRTK

---
+++ HoloLens Maker/Assets/Animate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class Animate : MonoBehaviour
+{
+    float rotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation += 36 * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(0, rotation, 0);
+    }
+}
